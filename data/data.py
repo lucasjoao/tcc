1b19afdf816b3a0a64de60a35c709cbcf5c434e8
lucas <lucasjoao.lj@gmail.com>
2020-04-13 10:22:41 -0300

    Write manual data from 'banco inter' files.

--- data/data.py
+++ data/data.py
@@ -1,2 +1,16 @@
-# TODO
-ROI = {'inter20192T': 10}
+# em milhoes
+LUCRO_LIQUIDO = {'inter20181T': 11.2,
+                 'inter20192T': 45,
+                 'inter20193T': 11.8
+                 'inter20194T': 24.7}
+
+# em milhoes
+PATRIMONIO_LIQUIDO = {'inter20181T': 387.4,
+                      'inter20192T': 973.1,
+                      'inter20193T': 2189.9,
+                      'inter20194T': 2200}
+
+ROE = {'inter20181T': 2.89,
+       'inter20192T': 4.62,
+       'inter20193T': 0.53,
+       'inter20194T': 1.12}
