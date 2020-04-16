3c63934639effb90499bda2fcdea03ae11aaaf26
lucas <lucasjoao.lj@gmail.com>
2020-04-15 23:55:08 -0300

    Manual data from 'gerdau' and 'itau'.

--- data/data.py
+++ data/data.py
@@ -1,16 +1,46 @@
 # em milhoes
-LUCRO_LIQUIDO = {'inter20181T': 11.2,
+LUCRO_LIQUIDO = {
+                 'gerdau20153T': -193,
+                 'gerdau20171T': 823.5,
+                 'gerdau20184T': 389,
+                 'gerdau20192T': 372.7,
+                 'inter20181T': 11.2,
                  'inter20192T': 45,
                  'inter20193T': 11.8,
-                 'inter20194T': 24.7}
+                 'inter20194T': 24.7,
+                 'itau20172T': 6792,
+                 'itau20181T': 6557,
+                 'itau20192T': 6690,
+                 'itau20193T': 5505
+              }
 
 # em milhoes
-PATRIMONIO_LIQUIDO = {'inter20181T': 387.4,
+PATRIMONIO_LIQUIDO = {
+                      'gerdau20153T': 36012.3,
+                      'gerdau20171T': 24663.4,
+                      'gerdau20184T': 25938.5,
+                      'gerdau20192T': 26568.4,
+                      'inter20181T': 387.4,
                       'inter20192T': 973.1,
                       'inter20193T': 2189.9,
-                      'inter20194T': 2200}
+                      'inter20194T': 2200,
+                      'itau20172T': 138492,
+                      'itau20181T': 136389,
+                      'itau20192T': 143813,
+                      'itau20193T': 143862
+                     }
 
-ROE = {'inter20181T': 2.89,
+ROE = {
+       'gerdau20153T': -0.53,
+       'gerdau20171T': 3.33,
+       'gerdau20184T': 1.49,
+       'gerdau20192T': 1.40,
+       'inter20181T': 2.89,
        'inter20192T': 4.62,
        'inter20193T': 0.53,
-       'inter20194T': 1.12}
+       'inter20194T': 1.12,
+       'itau20172T': 4.90,  # diff from fundamentus.com.br
+       'itau20181T': 4.88,
+       'itau20192T': 4.65,
+       'itau20193T': 3.82
+       }
