f12318fd616fcb4dd9c1b59d2b4f1c3dedeb5091
lucas <lucasjoao.lj@gmail.com>
2020-04-22 22:31:54 -0300

    Create data_dir_scan class that get relative dir and the filenames of 'data'.
    If this don't be used in the final project, at least will be used in
    tests.

---
+++ src/data_dir_scan.py
@@ -0,0 +1,15 @@
+import os
+
+class data_dir_scan:
+
+    @staticmethod
+    def get_data_directory():
+        data_directory = 'data/'
+        if 'src' in os.getcwd():
+            data_directory = '../data/'
+        return data_directory
+
+    @staticmethod
+    def get_files_name():
+        data_directory = data_dir_scan.get_data_directory()
+        return [item for item in os.listdir(data_directory) if '.pdf' in item]
