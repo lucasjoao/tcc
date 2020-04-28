ceb6b5f9389fea97010bb3b936409614e7b60e04
lucas <lucasjoao.lj@gmail.com>
2020-04-28 10:56:22 -0300

    Start class to roe_calculator.

---
+++ src/roe_calculator.py
@@ -0,0 +1,15 @@
+
+
+class roe_calculator:
+
+    def __init__(self, pdf_text):
+        self.pdf_text = pdf_text
+
+    def get_patrimonio_liquido(self):
+        pass
+
+    def get_lucro_liquido(self):
+        pass
+
+    def execute(self):
+        pass
