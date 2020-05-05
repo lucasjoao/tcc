bea5522001bc3188f7f9d3e378b3053f9ff33a8b
lucas <lucasjoao.lj@gmail.com>
2020-05-05 00:00:08 -0300

    Write doc and tests to data_dir_scan.py

--- src/data_dir_scan.py
+++ src/data_dir_scan.py
@@ -1,16 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Funcoes auxiliares para interagir com o diretorio 'data' do repositorio que possui os
+relatorios utilizados nos experimentos"""
+
 import os
 
 
 class data_dir_scan:
+    """Classe que possui as funcoes auxiliares"""
 
     @staticmethod
     def get_data_directory():
+        """Retorna uma string que contem o diretorio 'data' que possui os relatorios
+        com base na localizacao em que o metodo foi chamado
+
+        Returns:
+            String com o diretorio data
+        """
         data_directory = 'data/'
-        if 'src' in os.getcwd():
+        current_directory = os.getcwd()
+        if 'src' in current_directory or 'tests' in current_directory:
             data_directory = '../data/'
         return data_directory
 
     @staticmethod
     def get_files_name():
+        """Retorna os arquivos .PDFs presente no diretorio 'data'
+
+        Returns:
+            Lista de string com o nome dos arquivos .PDFs
+        """
         data_directory = data_dir_scan.get_data_directory()
         return [item for item in os.listdir(data_directory) if '.pdf' in item]
---
+++ tests/test_data_dir_scan.py
@@ -0,0 +1,30 @@
+
+import unittest
+from unittest import mock
+from src import data_dir_scan as dds
+
+
+class TestsDataDirScan(unittest.TestCase):
+
+    @mock.patch('os.getcwd')
+    def test_get_data_directory_from_root(self, mock_getcwd):
+        mock_getcwd.return_value = '/home/tcc'
+        self.assertEqual(dds.data_dir_scan.get_data_directory(), 'data/')
+        mock_getcwd.assert_called_once()
+
+    @mock.patch('os.getcwd')
+    def test_get_data_directory_from_src(self, mock_getcwd):
+        mock_getcwd.return_value = '/home/tcc/src'
+        self.assertEqual(dds.data_dir_scan.get_data_directory(), '../data/')
+        mock_getcwd.assert_called_once()
+
+    @mock.patch('os.getcwd')
+    def test_get_files_name_size(self, mock_getcwd):
+        mock_getcwd.return_value = '/home/tcc'
+        files_name = dds.data_dir_scan.get_files_name()
+        self.assertEqual(len(files_name), 20)
+        mock_getcwd.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
