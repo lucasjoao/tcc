bbcc702ff6573b0e23f57e8bad9a36d219c0561b
lucas <lucasjoao.lj@gmail.com>
2020-05-04 23:59:21 -0300

    Name test.

--- tests/test_src.py
+++ tests/test_src.py
@@ -3,7 +3,7 @@ import unittest
 from src import __version__
 
 
-class Tests(unittest.TestCase):
+class TestsSrc(unittest.TestCase):
 
     def test_version(self):
         self.assertEqual(__version__, '0.1.0')
