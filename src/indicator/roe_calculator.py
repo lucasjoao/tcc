e66161fb3b7ec4a3b5f1fb90daae287f58cfe9bf
lucas <lucasjoao.lj@gmail.com>
2020-05-13 20:25:34 -0300

    WIP: roe calculator.

--- src/indicator/roe_calculator.py
+++ src/indicator/roe_calculator.py
@@ -1,15 +1,24 @@
+# -*- coding: utf-8 -*-
+# from src.plataform import searcher as searcher
 
 
-class roe_calculator:
+class RoeCalculator:
 
     def __init__(self, pdf_text):
         self.pdf_text = pdf_text
 
     def get_patrimonio_liquido(self):
-        pass
+        return None
 
     def get_lucro_liquido(self):
-        pass
+        return None
 
     def execute(self):
+        # tentar buscar por roe com searcher (criar um searcher global)
+        # se não der, então buscar por patrimonio liquido e lucro liquido
+        # cada um com seu searcher
+        # se tiver ambos, entao calcular
+        # tenho que colocar o cleanup da string em algum lugar
+        # ler sobre empty constructor python, aqui deve vir o pdf_text
+        # gets nao possuem visibilidade publica, como?
         pass
---
+++ tests/test_roe_calculator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+# from src.indicator import roe_calculator as roe
+
+
+class TestRoeCalculator(unittest.TestCase):
+
+    def test_get_patrimonio_liquido(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
