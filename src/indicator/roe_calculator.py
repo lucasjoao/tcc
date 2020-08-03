24e47673e19bb90a72e091f999d22ae479e0f2d5
lucas <lucasjoao.lj@gmail.com>
2020-08-03 10:29:06 -0300

    RoeCalculator#execute impl without test.

--- src/indicator/roe_calculator.py
+++ src/indicator/roe_calculator.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
-# from src.plataform import searcher as searcher
+from src.plataform import searcher as searcher
 
 
 class RoeCalculator:
 
     def __init__(self, pdf_text):
         self.pdf_text = pdf_text
+        self.searcherRoe = searcher.Searcher('ROE', 'retorno on equity')
+        self.searcherPatrimonioLiquido = searcher.Searcher('patrimônio líquido')
+        self.searcherLucroLiquido = searcher.Searcher('lucro líquido')
 
     def get_patrimonio_liquido(self):
-        return None
+        return self.searcherPatrimonioLiquido.run(self.pdf_text)
 
     def get_lucro_liquido(self):
-        return None
+        return self.searcherLucroLiquido.run(self.pdf_text)
 
     def execute(self):
-        # tentar buscar por roe com searcher (criar um searcher global)
-        # se não der, então buscar por patrimonio liquido e lucro liquido
-        # cada um com seu searcher
-        # se tiver ambos, entao calcular
-        # tenho que colocar o cleanup da string em algum lugar
-        # ler sobre empty constructor python, aqui deve vir o pdf_text
-        # gets nao possuem visibilidade publica, como?
-        pass
+        roe_value = self.searcherRoe(self.pdf_text)
+        if roe_value is None:
+            lucro_liquido = self.get_lucro_liquido()
+            patrimonio_liquido = self.get_patrimonio_liquido()
+            if lucro_liquido is not None and patrimonio_liquido is not None:
+                roe_value = (lucro_liquido / patrimonio_liquido) * 100
+
+        return roe_value
