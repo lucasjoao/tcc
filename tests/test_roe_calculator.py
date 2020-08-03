61a9a5f872b7f834531e5336e2531950248e23c6
lucas <lucasjoao.lj@gmail.com>
2020-08-03 10:55:35 -0300

    WIP test to dummy data in roe calculator.

--- tests/test_roe_calculator.py
+++ tests/test_roe_calculator.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
 import unittest
-# from src.indicator import roe_calculator as roe
+from src.indicator import roe_calculator as roe
 
 
 class TestRoeCalculator(unittest.TestCase):
 
-    def test_get_patrimonio_liquido(self):
-        pass
+    def test_roe_found(self):
+        roe_calculator = roe.RoeCalculator('weg possui um ROE 32% em 2022')
+        result = roe_calculator.execute()
+        self.assertEqual(result, '32%')
+
+    def test_nothing_found(self):
+        roe_calculator = roe.RoeCalculator('esse texto dummy não possui nenhuma info para o indicador')
+        result = roe_calculator.execute()
+        self.assertIsNone(result)
+
+    def test_almost_nothing_found(self):
+        roe_calculator = roe.RoeCalculator('esse texto dummy só possui o lucro líquido 2.00')
+        result = roe_calculator.execute()
+        self.assertIsNone(result)
+
+    def test_calculation(self):
+        roe_calculator = roe.RoeCalculator('esse texto possui lucro líquido 2.00 e patrimônio líquido 78.00')
+        result = roe_calculator.execute()
+        self.assertEqual(result, 2.56)
+
+    # teste dado real para os mesmos itens anteriores
 
 
 if __name__ == '__main__':
