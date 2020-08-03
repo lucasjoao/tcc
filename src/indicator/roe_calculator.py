dfe325c3ca85fd19cdd3ae2c507a68fab8068e96
lucas <lucasjoao.lj@gmail.com>
2020-08-03 10:53:48 -0300

    Fix mistakes (init and execute) and wip convert string to number in gets.

--- src/indicator/roe_calculator.py
+++ src/indicator/roe_calculator.py
@@ -6,18 +6,25 @@ class RoeCalculator:
 
     def __init__(self, pdf_text):
         self.pdf_text = pdf_text
-        self.searcherRoe = searcher.Searcher('ROE', 'retorno on equity')
+        self.searcherRoe = searcher.Searcher(['ROE', 'retorno on equity'])
         self.searcherPatrimonioLiquido = searcher.Searcher('patrimônio líquido')
         self.searcherLucroLiquido = searcher.Searcher('lucro líquido')
 
     def get_patrimonio_liquido(self):
-        return self.searcherPatrimonioLiquido.run(self.pdf_text)
+        patrimonio_liquido = self.searcherPatrimonioLiquido.run(self.pdf_text)
+        if patrimonio_liquido is not None:
+            patrimonio_liquido = float(patrimonio_liquido)
+        return patrimonio_liquido
 
+    # TODO: testar isso e get_patrimonio_liquido?
     def get_lucro_liquido(self):
-        return self.searcherLucroLiquido.run(self.pdf_text)
+        lucro_liquido = self.searcherLucroLiquido.run(self.pdf_text)
+        if lucro_liquido is not None:
+            lucro_liquido = float(lucro_liquido)
+        return lucro_liquido
 
     def execute(self):
-        roe_value = self.searcherRoe(self.pdf_text)
+        roe_value = self.searcherRoe.run(self.pdf_text)
         if roe_value is None:
             lucro_liquido = self.get_lucro_liquido()
             patrimonio_liquido = self.get_patrimonio_liquido()
