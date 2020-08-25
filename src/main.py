846ad9a255184d002a3f8e58cd53e5480b94d4f5
lucas <lucasjoao.lj@gmail.com>
2020-08-24 23:53:16 -0300

    Change main.py to do new things after talk with elder.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 import PyPDF2
-from data_dir_scan import data_dir_scan
+import nltk
 
+from src.plataform import data_dir_scan as dds
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
-files_name = data_dir_scan.get_files_name()
-print(len(files_name))
+nltk.download('punkt')
+nltk.download('stopwords')
 
-for file_name in files_name:
-    pdf_reader = PyPDF2.PdfFileReader(data_dir_scan.get_data_directory() + file_name)
+pdf_reader = PyPDF2.PdfFileReader(dds.data_dir_scan.get_data_directory() + 'ambev_2019_1T.pdf')
+# pdf_reader = PyPDF2.PdfFileReader(dds.data_dir_scan.get_data_directory() + 'bancointer_2018_1T.pdf')
+# pdf_reader = PyPDF2.PdfFileReader(dds.data_dir_scan.get_data_directory() + 'gerdau_2019_2T.pdf')
+# pdf_reader = PyPDF2.PdfFileReader(dds.data_dir_scan.get_data_directory() + 'itau_2019_2T.pdf')
+# pdf_reader = PyPDF2.PdfFileReader(dds.data_dir_scan.get_data_directory() + 'weg_2019_2T.pdf')
 
-    pdf_text = ''
-    for i in range(pdf_reader.numPages):
-        pdf_page = pdf_reader.getPage(i)
-        pdf_text += pdf_page.extractText()
+pdf_text = ''
+for i in range(pdf_reader.numPages):
+    pdf_page = pdf_reader.getPage(i)
+    pdf_text += pdf_page.extractText()
 
-    print(pdf_text)
-    print(pdf_reader.numPages)
+text_tokens = word_tokenize(pdf_text)
+tokens_without_stopwords = [token for token in text_tokens if not token in stopwords.words('portuguese')]
+
+# print(tokens_without_stopwords)
+print(len(tokens_without_stopwords))
+print(len(text_tokens))
