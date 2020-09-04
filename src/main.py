fe729be2a0192b6b2801217fbef52acaac2f0967
lucas <lucasjoao.lj@gmail.com>
2020-09-03 22:01:26 -0300

    Found possible number to two domains: ['lucro líquido', 'patrimônio líquido'].
    'Lucro líquido' only works with weg.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import nltk
+import math
 
 from src.plataform import data_dir_scan as dds
 from nltk.tokenize import word_tokenize, sent_tokenize
@@ -54,9 +55,48 @@ def stemming(sentences_tokens):
 
     return stemming_result
 
-def roe_stemming():
+
+def lucro_liquido_stemming():
     stemmer = RSLPStemmer()
-    return frozenset([stemmer.stem('patrimônio'), stemmer.stem('lucro'), stemmer.stem('líquido')])
+    return frozenset([stemmer.stem('lucro'), stemmer.stem('líquido')])
+
+
+def patrimonio_liquido_stemming():
+    stemmer = RSLPStemmer()
+    return frozenset([stemmer.stem('patrimônio'), stemmer.stem('líquido')])
+
+
+def candidate_sentences(text, searcher_set):
+    candidates = []
+    for sentence in text:
+        if searcher_set < frozenset(sentence):
+            candidates.append(sentence)
+    return candidates
+
+
+def is_number(string):
+    try:
+        number = float(string.replace(',', '.'))
+        return (True, number)
+    except ValueError:
+        return (False, math.nan)
+
+
+def number_value_searcher(candidate_sentences):
+    invalid_positions = frozenset([0, 1])
+    results = []
+    for sentence in candidate_sentences:
+        position = 0
+        for token in sentence:
+            was_casted, number = is_number(token)
+            if was_casted and position not in invalid_positions:
+                if sentence[position - 2] == 'r' and sentence[position - 1] == '$':
+                    # FIXME: isso pode quebrar se for o último
+                    results.append({'number': number, 'possibleSize': sentence[position + 1]})
+
+            position += 1
+    return results
+
 
 def ner(tokens):
     # for token in tokens_without_stopwords:
@@ -70,4 +110,11 @@ pdf_reader = PyPDF2.PdfFileReader(dds.data_dir_scan.get_data_directory() + 'weg_
 pdf_text = extract_text(pdf_reader)
 preprocessed_text = preprocess(pdf_text)
 stemming_text = stemming(preprocessed_text)
-print(roe_stemming())
+ll_stemming_set = lucro_liquido_stemming()
+pl_stemming_set = patrimonio_liquido_stemming()
+
+ll_candidate_sentences = candidate_sentences(stemming_text, ll_stemming_set)
+pl_candidate_sentences = candidate_sentences(stemming_text, pl_stemming_set)
+
+print(number_value_searcher(ll_candidate_sentences))
+print(number_value_searcher(pl_candidate_sentences))
