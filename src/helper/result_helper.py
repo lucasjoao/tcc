afb31c0ad61991d9e6de4c1da5dae4cff1dad5ac
lucas <lucasjoao.lj@gmail.com>
2020-10-03 19:35:34 -0300

    Create constant to 'undefined'

--- src/helper/result_helper.py
+++ src/helper/result_helper.py
@@ -3,13 +3,15 @@ from src.helper import number_helper as nh
 
 class result_helper:
 
+    __undefined = 'undefined'
+
     @staticmethod
     def create_result_item(number, possible_size):
         normalized_possible_size = possible_size
         was_casted, _ = nh.number_helper.is_number(possible_size)
 
         if was_casted:
-            normalized_possible_size = 'undefined'  # 'undefined' rather than None here solve some problems with types
+            normalized_possible_size = result_helper.__undefined  # not None here solve some problems with types
 
         return {'number': number, 'possible_size': normalized_possible_size}
 
@@ -17,6 +19,6 @@ class result_helper:
     def clean_search_result(dirty_result):
         clean_result = []
         for dict_result in dirty_result:
-            if dict_result['possible_size'] != 'undefined':
+            if dict_result['possible_size'] != result_helper.__undefined:
                 clean_result.append(dict_result)
         return clean_result
