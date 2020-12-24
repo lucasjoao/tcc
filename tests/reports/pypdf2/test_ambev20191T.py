c0efd6fe3d2a8eb74dc6523607bfae8373d89f15
lucas <lucasjoao.lj@gmail.com>
2020-12-24 11:53:31 -0300

    Finish separation to folder only run test with pypdf2

--- tests/reports/pypdf2/test_ambev20191T.py
+++ tests/reports/pypdf2/test_ambev20191T.py
@@ -8,98 +8,62 @@ class TestsAmbev20191T(unittest.TestCase):
 
     filename = None
     manager_pypdf2 = None
-    manager_pytesseract = None
 
     @classmethod
     def setUpClass(cls):
         cls.filename = 'ambev_2019_1T.pdf'
-        # cls.manager_pypdf2 = m.manager([cls.filename], 'pypdf2')
-        cls.manager_pytesseract = m.manager([cls.filename], 'pypdf2')
+        cls.manager_pypdf2 = m.manager([cls.filename], 'pypdf2')
 
     @classmethod
     def tearDownClass(cls):
         cls.filename = None
-        # cls.manager_pypdf2 = None
-        cls.manager_pytesseract = None
+        cls.manager_pypdf2 = None
 
     def test_lucro_liquido_monetary(self):
         lucro_liquido_monetary_pypdf2 = self.manager_pypdf2.run_lucro_liquido_monetary()
         result_pypdf2 = lucro_liquido_monetary_pypdf2[self.filename]
         numbers_from_result_pypdf2 = rh.result_helper.get_numbers_as_list(result_pypdf2)
 
-        lucro_liquido_monetary_pytesseract = self.manager_pytesseract.run_lucro_liquido_monetary()
-        result_pytesseract = lucro_liquido_monetary_pytesseract[self.filename]
-        numbers_from_result_pytesseract = rh.result_helper.get_numbers_as_list(result_pytesseract)
-
         self.assertEqual(len(result_pypdf2), 2, 'lucro líquido (R$): tamanho resultado (pypdf2)')
         self.assertNotIn(data.LUCRO_LIQUIDO[self.filename], numbers_from_result_pypdf2,
                          'lucro líquido (R$): valor (pypdf2)')
-        self.assertEqual(len(result_pytesseract), 3, 'lucro líquido (R$): tamanho resultado (pytesseract)')
-        self.assertNotIn(data.LUCRO_LIQUIDO[self.filename], numbers_from_result_pytesseract,
-                         'lucro líquido (R$): valor (pytesseract)')
 
     def test_lucro_liquido_number(self):
-        # lucro_liquido_number_pypdf2 = self.manager_pypdf2.run_lucro_liquido_number()
-        # result_pypdf2 = lucro_liquido_number_pypdf2[self.filename]
-        # numbers_from_result_pypdf2 = rh.result_helper.get_numbers_as_list(result_pypdf2)
+        lucro_liquido_number_pypdf2 = self.manager_pypdf2.run_lucro_liquido_number()
+        result_pypdf2 = lucro_liquido_number_pypdf2[self.filename]
 
-        lucro_liquido_number_pytesseract = self.manager_pytesseract.run_lucro_liquido_number()
-        result_pytesseract = lucro_liquido_number_pytesseract[self.filename]
-        numbers_from_result_pytesseract = rh.result_helper.get_numbers_as_list(result_pytesseract)
-
-        # self.assertEqual(len(result_pypdf2), 0, 'lucro líquido (número após conjunto de busca): tamanho resultado (pypdf2)')
-        # WHY:???????
-        self.assertEqual(len(result_pytesseract), 0,
-                         'lucro líquido (número após conjunto de busca): tamanho resultado (pytesseract)')
+        self.assertEqual(len(result_pypdf2), 0, 'lucro líquido (número após conjunto de busca): tamanho resultado')
 
     def test_patrimonio_liquido_monetary(self):
         patrimonio_liquido_monetary = self.manager_pypdf2.run_patrimonio_liquido_monetary()
         result = patrimonio_liquido_monetary[self.filename]
 
         self.assertEqual(len(result), 0, 'patrimônio líquido (R$): tamanho resultado')
-        self.assertEqual(result,
-                         self.manager_pytesseract.run_patrimonio_liquido_monetary()[self.filename],
-                         'ambas libs trazem o mesmo resultado')
 
     def test_patrimonio_liquido_number(self):
         patrimonio_liquido_number_pypdf2 = self.manager_pypdf2.run_patrimonio_liquido_number()
         result_pypdf2 = patrimonio_liquido_number_pypdf2[self.filename]
-        patrimonio_liquido_number_pytesseract = self.manager_pytesseract.run_patrimonio_liquido_number()
-        result_pytesseract = patrimonio_liquido_number_pytesseract[self.filename]
 
         self.assertEqual(len(result_pypdf2), 0,
                          'patrimônio líquido (número após conjunto de busca): tamanho resultado (pypdf2)')
-        # VERIFICAR QUE PROXIMO NAO VAI
-        self.assertEqual(len(result_pytesseract), 0,
-                         'patrimônio líquido (número após conjunto de busca): tamanho resultado (pytesseract)')
 
     def test_roe_monetary(self):
         roe_monetary = self.manager_pypdf2.run_roe_monetary()
         result = roe_monetary[self.filename]
 
         self.assertEqual(len(result), 0, 'ROE (R$): tamanho resultado')
-        self.assertEqual(result,
-                         self.manager_pytesseract.run_roe_monetary()[self.filename],
-                         'ambas libs trazem o mesmo resultado')
 
     def test_roe_number(self):
         roe_number = self.manager_pypdf2.run_roe_number()
         result = roe_number[self.filename]
 
         self.assertEqual(len(result), 0, 'ROE (número após conjunto de busca): tamanho resultado')
-        self.assertEqual(result,
-                         self.manager_pytesseract.run_roe_number()[self.filename],
-                         'ambas libs trazem o mesmo resultado')
 
     def test_roe_calculate(self):
         calculate_roe_pypdf2 = self.manager_pypdf2.run_calculate_roe()
         result_pypdf2 = calculate_roe_pypdf2[self.filename]
-        calculate_roe_pytesseract = self.manager_pytesseract.run_calculate_roe()
-        result_pytesseract = calculate_roe_pytesseract[self.filename]
 
         self.assertEqual(len(result_pypdf2), 0, 'ROE por cálculo: tamanho resultado (pypdf2)')
-        # VERIFICAR QUE PROXIMO NAO ROLA
-        self.assertEqual(len(result_pytesseract), 0, 'ROE por cálculo: tamanho resultado (pytesseract)')
 
 
 if __name__ == '__main__':
--- tests/reports/pypdf2/test_gerdau20192T.py
+++ tests/reports/pypdf2/test_gerdau20192T.py
@@ -8,92 +8,62 @@ class TestsGerdau20192T(unittest.TestCase):
 
     filename = None
     manager_pypdf2 = None
-    manager_pytesseract = None
 
     @classmethod
     def setUpClass(cls):
         cls.filename = 'gerdau_2019_2T.pdf'
         cls.manager_pypdf2 = m.manager([cls.filename], 'pypdf2')
-        cls.manager_pytesseract = m.manager([cls.filename], 'pytesseract')
 
     @classmethod
     def tearDownClass(cls):
         cls.filename = None
         cls.manager_pypdf2 = None
-        cls.manager_pytesseract = None
 
     def test_lucro_liquido_monetary(self):
         lucro_liquido_monetary = self.manager_pypdf2.run_lucro_liquido_monetary()
         result = lucro_liquido_monetary[self.filename]
 
         self.assertEqual(len(result), 0, 'lucro líquido (R$): tamanho resultado')
-        self.assertEqual(result,
-                         self.manager_pytesseract.run_lucro_liquido_monetary()[self.filename],
-                         'ambas libs trazem o mesmo resultado')
 
     def test_lucro_liquido_number(self):
         lucro_liquido_number_pypdf2 = self.manager_pypdf2.run_lucro_liquido_number()
         result_pypdf2 = lucro_liquido_number_pypdf2[self.filename]
         numbers_from_result_pypdf2 = rh.result_helper.get_numbers_as_list(result_pypdf2)
 
-        lucro_liquido_number_pytesseract = self.manager_pytesseract.run_lucro_liquido_number()
-        result_pytesseract = lucro_liquido_number_pytesseract[self.filename]
-        numbers_from_result_pytesseract = rh.result_helper.get_numbers_as_list(result_pytesseract)
-
-        self.assertEqual(len(result_pypdf2), 1, 'lucro líquido (número após conjunto de busca): tamanho resultado (pypdf2)')
+        self.assertEqual(len(result_pypdf2), 1, 'lucro líquido (número após conjunto de busca): tamanho resultado')
         self.assertNotIn(data.LUCRO_LIQUIDO[self.filename], numbers_from_result_pypdf2,
                          'lucro líquido (número após conjunto de busca): valor (pypdf2)')
-        self.assertEqual(len(result_pytesseract), 1,
-                         'lucro líquido (número após conjunto de busca): tamanho resultado (pytesseract)')
-        self.assertNotIn(data.LUCRO_LIQUIDO[self.filename], numbers_from_result_pytesseract,
-                         'lucro líquido (número após conjunto de busca): valor (pytesseract)')
 
     def test_patrimonio_liquido_monetary(self):
         patrimonio_liquido_monetary = self.manager_pypdf2.run_patrimonio_liquido_monetary()
         result = patrimonio_liquido_monetary[self.filename]
 
         self.assertEqual(len(result), 0, 'patrimônio líquido (R$): tamanho resultado')
-        self.assertEqual(result,
-                         self.manager_pytesseract.run_patrimonio_liquido_monetary()[self.filename],
-                         'ambas libs trazem o mesmo resultado')
 
     def test_patrimonio_liquido_number(self):
         patrimonio_liquido_number_pypdf2 = self.manager_pypdf2.run_patrimonio_liquido_number()
         result_pypdf2 = patrimonio_liquido_number_pypdf2[self.filename]
-        patrimonio_liquido_number_pytesseract = self.manager_pytesseract.run_patrimonio_liquido_number()
-        result_pytesseract = patrimonio_liquido_number_pytesseract[self.filename]
 
         self.assertEqual(len(result_pypdf2), 0,
                          'patrimônio líquido (número após conjunto de busca): tamanho resultado (pypdf2)')
-        self.assertEqual(len(result_pytesseract), 0,
-                         'patrimônio líquido (número após conjunto de busca): tamanho resultado (pytesseract)')
 
     def test_roe_monetary(self):
         roe_monetary = self.manager_pypdf2.run_roe_monetary()
         result = roe_monetary[self.filename]
 
         self.assertEqual(len(result), 0, 'ROE (R$): tamanho resultado')
-        self.assertEqual(result,
-                         self.manager_pytesseract.run_roe_monetary()[self.filename],
-                         'ambas libs trazem o mesmo resultado')
 
     def test_roe_number(self):
         roe_number = self.manager_pypdf2.run_roe_number()
         result = roe_number[self.filename]
 
         self.assertEqual(len(result), 0, 'ROE (número após conjunto de busca): tamanho resultado')
-        self.assertEqual(result,
-                         self.manager_pytesseract.run_roe_number()[self.filename],
-                         'ambas libs trazem o mesmo resultado')
 
     def test_roe_calculate(self):
         calculate_roe_pypdf2 = self.manager_pypdf2.run_calculate_roe()
         result_pypdf2 = calculate_roe_pypdf2[self.filename]
-        calculate_roe_pytesseract = self.manager_pytesseract.run_calculate_roe()
-        result_pytesseract = calculate_roe_pytesseract[self.filename]
 
         self.assertEqual(len(result_pypdf2), 0, 'ROE por cálculo: tamanho resultado (pypdf2)')
-        self.assertEqual(len(result_pytesseract), 0, 'ROE por cálculo: tamanho resultado (pytesseract)')
 
 
 if __name__ == '__main__':
