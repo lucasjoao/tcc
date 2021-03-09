b5825129fb844a12a3bcb244e21905004e7cfa4b
lucas <lucasjoao.lj@gmail.com>
2021-03-08 22:56:03 -0300

    Refactory data_dir_scan
    This was necessary because when I wrote about this in the text I saw
    some mistakes in this class.

--- src/plataform/data_dir_scan.py
+++ src/plataform/data_dir_scan.py
@@ -1,25 +1,28 @@
-# -*- coding: utf-8 -*-
-"""Funcoes auxiliares para interagir com o diretorio 'data' do repositorio que possui os
-relatorios utilizados nos experimentos"""
-
 import os
 
 
 class data_dir_scan:
-    """Classe que possui as funcoes auxiliares"""
+
+    __src = 'src'
+    __tests = 'tests'
 
     @staticmethod
     def get_data_directory():
-        """Retorna uma string que contem o diretorio 'data' que possui os relatorios
-        com base na localizacao em que o metodo foi chamado
-
-        Returns:
-            String com o diretorio data
-        """
-        data_directory = 'data/'
         current_directory = os.getcwd()
-        if 'src' in current_directory:
-            data_directory = '../data/'
-        if 'tests' in current_directory:
-            data_directory = '../../../data/'
-        return data_directory
+        directory_splitted = current_directory.split('/')
+
+        if 'data' == directory_splitted[-1]:  # last element from list
+            return './'
+
+        directory_founded = None
+        if data_dir_scan.__src in directory_splitted:
+            directory_founded = data_dir_scan.__src
+        elif data_dir_scan.__tests in current_directory:
+            directory_founded = data_dir_scan.__tests
+
+        if directory_founded is None:
+            raise Exception('Diretório inválido!')
+
+        position = directory_splitted.index(directory_founded)
+        levels_below = len(directory_splitted) - position
+        return ('../' * levels_below) + 'data/'
--- tests/plataform/test_data_dir_scan.py
+++ tests/plataform/test_data_dir_scan.py
@@ -9,13 +9,25 @@ class TestsDataDirScan(unittest.TestCase):
     @mock.patch('os.getcwd')
     def test_get_data_directory_from_root(self, mock_getcwd):
         mock_getcwd.return_value = '/home/tcc'
-        self.assertEqual(dds.data_dir_scan.get_data_directory(), 'data/')
+        self.assertRaises(Exception, dds.data_dir_scan.get_data_directory)
         mock_getcwd.assert_called_once()
 
     @mock.patch('os.getcwd')
-    def test_get_data_directory_from_src(self, mock_getcwd):
-        mock_getcwd.return_value = '/home/tcc/src'
+    def test_get_data_directory_from_tests(self, mock_getcwd):
+        mock_getcwd.return_value = '/home/tcc/tests'
         self.assertEqual(dds.data_dir_scan.get_data_directory(), '../data/')
+        mock_getcwd.assert_called_once()
+
+    @mock.patch('os.getcwd')
+    def test_get_data_directory_from_src_with_levels_belows(self, mock_getcwd):
+        mock_getcwd.return_value = '/home/tcc/src/plataform'
+        self.assertEqual(dds.data_dir_scan.get_data_directory(), '../../data/')
+        mock_getcwd.assert_called_once()
+
+    @mock.patch('os.getcwd')
+    def test_get_data_directory_from_data(self, mock_getcwd):
+        mock_getcwd.return_value = '/home/tcc/data'
+        self.assertEqual(dds.data_dir_scan.get_data_directory(), './')
         mock_getcwd.assert_called_once()
 
 
