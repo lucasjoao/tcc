a557e1999c40711d4408c0f90e5a169a353b9b6a
lucas <lucasjoao.lj@gmail.com>
2021-03-30 11:55:18 -0300

    Finish tests to filters#candidate_sentences

--- tests/plataform/test_filters.py
+++ tests/plataform/test_filters.py
@@ -7,19 +7,23 @@ class TestsFilters(unittest.TestCase):
 
     def test_candidate_sentences_all_empty(self):
         filters = f.filters()
-        result = filters.candidate_sentences([''], [frozenset([])])
+        result = filters.candidate_sentences([['']], [frozenset([])])
         self.assertEqual(len(result), 0)
 
     def test_candidate_sentences_not_subset(self):
         filters = f.filters()
-        result = filters.candidate_sentences(['Ameixa', 'Ameixa', 'Abacate'], [frozenset(['teste', 'unitário'])])
+        result = filters.candidate_sentences([['Ameixa'], ['Ameixa'], ['Abacate']], [frozenset(['teste', 'unitário'])])
         self.assertEqual(len(result), 0)
 
-    # NEED FIX
     def test_candidate_sentences_subset(self):
         filters = f.filters()
-        result = filters.candidate_sentences(['Ameixa muito saborosa', 'Ameixa', 'Abacate'], [frozenset(['saborosa'])])
+        list_of_expected = ['Ameixa', 'muito', 'saborosa']
+
+        result = filters.candidate_sentences([list_of_expected, ['Ameixa'], ['Abacate']], [frozenset(['saborosa'])])
+        expected = [list_of_expected]
+
         self.assertEqual(len(result), 1)
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
