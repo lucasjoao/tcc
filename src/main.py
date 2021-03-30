5d7e51c206776b5983b90293af9ad2885989fa9c
lucas <lucasjoao.lj@gmail.com>
2021-03-29 23:03:09 -0300

    Linter fix

--- src/main.py
+++ src/main.py
@@ -15,4 +15,3 @@ text_example = "Eu gosto de abacate. Também gosto de tangerina."
 text_example_preprocessed = preprocessor.execute(text_example)
 print(text_example_preprocessed)
 print(stemmer.stem_text_matrix(text_example_preprocessed))
-
